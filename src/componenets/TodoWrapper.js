f10f9faef53e69023250bb885e74fc7b5c84db82
Ed Thomas <edthomas@eds-macbook-pro.home>
2024-05-23 17:22:14 +0100

    feat: edit the task

--- src/componenets/TodoWrapper.js
+++ src/componenets/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
 import { Todo } from './Todo'
+import { EditTodoForm } from './EditTodoForm'
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([])
@@ -19,12 +20,24 @@ export const TodoWrapper = () => {
       todo.id !== id 
     ))
   }
+  const editTodo = id => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+    ))
+  }
+  const editTask = (task, id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, isEditing: !todo.isEditing, task } : todo
+    ))
+  }
   return (
     <div className='TodoWrapper'>
       <h1>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => {
-        return <Todo task={todo} index={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+        return todo.isEditing ?
+          <EditTodoForm editTask={editTask} task={todo} /> :
+          <Todo task={todo} index={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
       })}
     </div> 
   )
